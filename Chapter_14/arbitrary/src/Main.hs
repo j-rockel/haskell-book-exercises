a368277ef0f013b6412f938857fb703369402f00
Jessika Rockel <jessika.rockel@snpgroup.com>
2021-06-14 17:24:19 +0200

    Chapter 14 WIP

--- Chapter_14/arbitrary/src/Main.hs
+++ Chapter_14/arbitrary/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
 
 data Trivial =
   Trivial
@@ -32,3 +33,39 @@ instance Arbitrary a => Arbitrary (Identity a) where
   
 identityGenInt :: Gen (Identity Int)
 identityGenInt = identityGen
+
+-- 
+
+data Pair a b =
+  Pair a b
+  deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+-- 
+
+data Sum a b =
+    First a
+  | Second b
+  deriving (Eq, Show)
+
+-- equal odds for each
+sumGenEqual :: (Arbitrary a, Arbitrary b) => Gen (Sum a b)
+sumGenEqual = do
+  a <- arbitrary
+  b <- arbitrary
+  oneof [return $ First a,
+         return $ Second b]
+
+sumGenCharInt :: Gen (Sum Char Int)
+sumGenCharInt = sumGenEqual
